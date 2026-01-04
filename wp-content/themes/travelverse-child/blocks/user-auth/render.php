ebc4f166f9f5d7dd172982603f32b1c8a4ac4422
Anak Agung Gede Putu Wiradarma <gungwira@Anaks-MacBook-Air.local>
2026-01-04 17:48:35 +0800

    feat(login): update login,register, and logout url

--- wp-content/themes/travelverse-child/blocks/user-auth/render.php
+++ wp-content/themes/travelverse-child/blocks/user-auth/render.php
@@ -1,20 +1,23 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+$account_url = site_url('/my-account');
+$logout_url = wp_logout_url( site_url('/my-account') );
+
 if ( is_user_logged_in() ) {
     $user = wp_get_current_user();
     ?>
     <div class="tv-user-auth logged-in">
         <img src="<?php echo esc_url( get_avatar_url( $user->ID ) ); ?>" class="tv-avatar" />
         <span class="tv-username"><?php echo esc_html( $user->display_name ); ?></span>
-        <a href="<?php echo esc_url( wp_logout_url() ); ?>" class="tv-logout">Logout</a>
+        <a href="<?php echo esc_url( $logout_url ); ?>" class="tv-logout">Logout</a>
     </div>
     <?php
 } else {
     ?>
     <div class="tv-user-auth logged-out">
-        <a href="<?php echo esc_url( wp_login_url() ); ?>" class="tv-login">Login</a>
-        <a href="<?php echo esc_url( wp_registration_url() ); ?>" class="tv-register">Register</a>
+        <a href="<?php echo esc_url( $account_url ); ?>" class="tv-login">Login</a>
+        <a href="<?php echo esc_url( $account_url ); ?>" class="tv-register">Register</a>
     </div>
     <?php
 }
